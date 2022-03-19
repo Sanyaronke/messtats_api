85ee125cc5a56593b8c5a3769bf51a0af2224ed2
ANYARONKE DARE SAMUEL <dsanyaronke@gmail.com>
2022-03-19 17:55:05 +0100

    int passport auth

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -10,6 +10,10 @@ use Illuminate\Http\JsonResponse;
 
 class UserController extends Controller
 {
+
+    public function __construct() {
+        $this->middleware('auth:api');
+    }
     /**
      * Display a listing of the resource.
      *
