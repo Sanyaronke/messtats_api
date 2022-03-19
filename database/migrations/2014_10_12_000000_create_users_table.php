e08fcdad129e3daa9cb372d8e76f5e21feb11450
ANYARONKE DARE SAMUEL <dsanyaronke@gmail.com>
2022-03-19 09:15:08 +0100

    first migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,9 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->boolean('sexe');
+            $table->date('birthday');
+            $table->string('size')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
