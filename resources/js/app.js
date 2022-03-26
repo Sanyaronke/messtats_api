ad98260a49c4fa54b0a254269a44b0e275dd965b
ANYARONKE DARE SAMUEL <dsanyaronke@gmail.com>
2022-03-26 14:13:33 +0100

    #1 Laravel braze pour du Vuejs

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,7 @@
 require('./bootstrap');
-import 'bootstrap/dist/css/bootstrap.css'
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router';
 
-createApp(App).use(router).mount('#app');
-import 'bootstrap/dist/js/bootstrap.js'
+import Alpine from 'alpinejs';
+
+window.Alpine = Alpine;
+
+Alpine.start();
